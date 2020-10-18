3ef563a33f24f9dfc56a35681c4f282e434b3ceb
Dmitry Urin <domainlover@yahoo.com>
2020-10-18 22:50:44 +0300

    File added

---
+++ Miles.java
@@ -0,0 +1,8 @@
+public class Miles {
+    public static void main(String[] args) {
+        int price = 21000;
+        int mileprice = 20;
+        int miles = price/mileprice;
+        System.out.println(miles);
+    }
+}
